657c25c8f79d787fa32c8017f866f919e4ff3535
Sanjeev Kumar <sanjeev@nmsworks.co.in>
2025-08-06 18:04:21 +0530

    My First Program
    Committing my HelloWorld Program

---
+++ sanjeev/src/main/java/in/co/nmsworks/week1/day1/HelloWorld.java
@@ -0,0 +1,19 @@
+package in.co.nmsworks.week1.day1;
+
+/**
+ * HelloWorld.java
+ * <p>
+ * Module in.co.nmsworks
+ * <p>
+ * Author K. Sanjeev Kumar (sanjeev@nmsworks.co.in)
+ * <p>
+ * Created  06-Aug-2025
+ **/
+public class HelloWorld
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World !!");
+    }
+}
