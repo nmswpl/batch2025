d42c0416a0769663726de9d228d038195454ac8a
kavi <kavim@nmsworks.co.in>
2025-08-08 18:02:28 +0530

    day3 work

---
+++ kavi/src/main/java/in/co/nmsworks/week1/day3/Candidate.java
@@ -0,0 +1,66 @@
+package in.co.nmsworks.week1.day3;
+
+public class Candidate {
+    private int id;
+    private String name;
+    private String email;
+    private String department;
+
+
+    public Candidate() {
+
+    }
+
+    public Candidate(int id, String name, String email, String department) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.department = department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void printCandidateInfo(){
+
+    }
+
+    public String toString()
+    {
+        return "Candidate{" +
+                "id=" + id +
+                ", name='" + name +
+                ", email='" + email +
+                ", department='" + department  +
+                '}';
+    }
+}
---
+++ kavi/src/main/java/in/co/nmsworks/week1/day3/Planet.java
@@ -0,0 +1,45 @@
+package in.co.nmsworks.week1.day3;
+
+public class Planet {
+    String planet;
+    int size;
+    String color;
+    int numberOfMoon;
+
+    public Planet(){
+
+    }
+    public void setPlanet(String palnet){
+        this.planet=planet;
+
+    }
+    public String getPlanet(){
+
+        return planet;
+    }
+    public void setSize(int Size){
+        this.size=size;
+    }
+    public int getSize(){
+
+        return size;
+    }
+    public void setColor(String color){
+        this.color=color;
+    }
+    public String getColor(){
+
+        return color;
+    }
+    public void setNumberOfMoon(int numberOfMoon){
+
+        this.numberOfMoon=numberOfMoon;
+    }
+    public int getNumberOfMoon(){
+
+        return numberOfMoon;
+    }
+
+
+
+}
