3ba587bed9fb760b42dc60ca4e7e0b86881624c2
Yamini <yamini@nmsworks.co.in>
2025-08-11 16:17:18 +0530

    test programs

--- yamini/src/main/java/in/co/nmsworks/week1/test/PrintTheHundredthPosition.java
+++ yamini/src/main/java/in/co/nmsworks/week1/test/PrintTheHundredthPosition.java
@@ -1,20 +1,13 @@
 package in.co.nmsworks.week1.test;
 
-import javax.swing.plaf.synth.SynthTabbedPaneUI;
-
 public class PrintTheHundredthPosition {
     public static void main(String[] args) {
         print(542);
         print(777);
-
     }
-
-    public  static void print(int number){
-        System.out.println(number/100);
-
+    public static void print(int number) {
+        String[] arr = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+        int digit = number/100;
+        System.out.println(arr[digit]);
     }
-
-
-
-
 }
--- yamini/src/main/java/in/co/nmsworks/week1/test/PrintingThePositionFirstAndLastCharacter.java
+++ yamini/src/main/java/in/co/nmsworks/week1/test/PrintingThePositionFirstAndLastCharacter.java
@@ -8,9 +8,10 @@ public class PrintingThePositionFirstAndLastCharacter {
         int index=input.indexOf(word.toLowerCase());
         System.out.println("Word found at index: "+index);
         System.out.println("First character of the word: "+input.charAt(index));
-        System.out.println("Last character of the word: "+input.charAt(word.length()));
-
-
-
+        //System.out.println(word.charAt(word.length()-1));
+        for(int i=word.length()-1;i>=0;i--){
+            System.out.println("Last character of the word:  "+word.charAt(i));
+            break;
+        }
     }
 }
