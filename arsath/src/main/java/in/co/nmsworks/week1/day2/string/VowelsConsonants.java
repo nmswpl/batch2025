e7d59548cc30af2804c804c41f62ffdc833688d9
Arsath2309 <ahamedarsath457@gmail.com>
2025-08-11 09:14:30 +0530

    Revert "Day 2 Programming"
    This reverts commit 435078ba90f91f341ce7d3422cad81e4a796b9ea.

--- arsath/src/main/java/in/co/nmsworks/week1/day2/string/VowelsConsonants.java
+++ arsath/src/main/java/in/co/nmsworks/week1/day2/string/VowelsConsonants.java
@@ -1,24 +1,23 @@
 package in.co.nmsworks.week1.day2.string;
 
 public class VowelsConsonants {
-    public static void printVowelsConsonants(String input) {
-        int vowels = 0, consonants = 0;
-        String ref = "aeiouAEIOU";
-        for (int i = 0; i < input.length(); i++) {
-            if ((input.charAt(i) >= 'A'
-                    && input.charAt(i) <= 'Z')
-                    || (input.charAt(i) >= 'a'
-                    && input.charAt(i) <= 'z')) {
-                if (ref.indexOf(input.charAt(i)) != -1)
-                    vowels++;
-                else
-                    consonants++;
+    public static void printVowelsConsonants(String input){
+        input = input.toLowerCase();
+        int vowels = 0;
+        int consonants = 0;
+        String[] vowels = new String[] {'a', 'e', ''}
+        for(int i=0;i<=input.length();i++){
+            if(input.contains("")){
+                vowels++;
+            }
+            else{
+                consonants++;
             }
         }
-        System.out.println("Number of Vowels = " + vowels
-                + "\nNumber of Consonants = "
-                + consonants);
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants:" + consonants);
     }
+
     public static void main(String[] args) {
         String input = "Hello World";
         printVowelsConsonants(input);
