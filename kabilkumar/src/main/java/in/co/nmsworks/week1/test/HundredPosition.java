c8b0124ce5a1fcf6aa14203182e58edc2aeaa895
kabilkumar <kabilkumark@nmsworks.co.in>
2025-08-11 11:26:09 +0530

    my test

---
+++ kabilkumar/src/main/java/in/co/nmsworks/week1/test/HundredPosition.java
@@ -0,0 +1,15 @@
+/*
+package in.co.nmsworks.week1.test;
+
+
+public class HundredPosition {
+    public static void main(String[] args) {
+        int num=123 ;
+        convertHundreds(num);
+    }
+
+    public static void convertHundreds(int data);
+
+    {}
+    }
+*/
