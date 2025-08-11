31cf9936be75bbcd2bdd1a111fab0a10e98fbc50
saranraji <saranraji@nmsworks.co.in>
2025-08-11 18:06:32 +0530

    today Learning Topic Object paramter

--- saranraj/src/main/java/in/co/nmsworks/runner/Runner.java
+++ saranraj/src/main/java/in/co/nmsworks/runner/Runner.java
@@ -1,11 +1,35 @@
 package in.co.nmsworks.runner;
 
-import in.co.nmsworks.week1.day3.Candidate;
+import in.co.nmsworks.week2.day1.Subject;
+import in.co.nmsworks.week2.day1.Teacher;
 
 public class Runner {
 
     public static void main(String[] args) {
 
+        Teacher t1 = new Teacher(1,"qwerty");
+        Teacher t2 = new Teacher(2,"hello");
+
+        System.out.println(t1);
+        System.out.println(t2);
+
+        Subject maths = new Subject();
+        maths.setCode("MA101");
+        maths.setName("Maths");
+        maths.setTeacher(t1);
+
+        System.out.println(maths);
+
+        Subject science = new Subject();
+        science.setCode("SE101");
+        science.setName("Science");
+        science.setTeacher(t2);
+        System.out.println(science);
+        printTeacher(science);
+
+    }
+    public static void printTeacher(Subject science){
+        System.out.println(science.getTeacher().getName());
     }
 
 }
---
+++ saranraj/src/main/java/in/co/nmsworks/week2/day1/Subject.java
@@ -0,0 +1,44 @@
+package in.co.nmsworks.week2.day1;
+
+public class Subject {
+    private String code;
+    private String name;
+
+   private Teacher teacher;
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public Subject() {
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Subject{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", teacher=" + teacher +
+                '}';
+    }
+}
