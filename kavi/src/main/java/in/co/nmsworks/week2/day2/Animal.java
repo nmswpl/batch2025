7145012d8aea091cc47bc0080ede13ae689744c8
kavi <kavim@nmsworks.co.in>
2025-08-12 16:06:13 +0530

    AnimalProgram

---
+++ kavi/src/main/java/in/co/nmsworks/week2/day2/Animal.java
@@ -0,0 +1,14 @@
+package in.co.nmsworks.week2.day2;
+
+public abstract class Animal {
+
+    public abstract String  makeSound();
+}
+class Dog extends Animal {
+    public String makeSound(){
+        return " ";
+    }
+
+
+}
+
