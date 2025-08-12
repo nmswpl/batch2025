d74f0e7c32287e15f35b764388fe05f624ef2bd5
Madhanakumar <madhanakumart@nmsworks.co.in>
2025-08-12 17:54:25 +0530

    Week 2 Day 2 programs
    Comitting the array example programs

---
+++ madhanakumar/src/main/java/in/co/nmsworks/week2/day2/Candidate.java
@@ -0,0 +1,51 @@
+package in.co.nmsworks.week2.day2;
+
+public class Candidate {
+
+    public Candidate(int id, String name, String mail, String deparment) {
+    }
+    public Candidate() {
+
+    }
+
+    public static void main(String[] args) {
+       Candidate candidate = new Candidate();
+       candidate.arrayExample();
+    }
+
+    public void arrayExample(){
+        Candidate c21 = new Candidate(21, "Arunkumar M","gajendrans@nmsworks.co.in","QA");
+        Candidate c22 = new Candidate(22, "Vasanth S","gajendrans@nmsworks.co.in","Dev");
+        Candidate c23 = new Candidate(23, "Nandhakumar M","gajendrans@nmsworks.co.in","QA");
+        Candidate c24 = new Candidate(24, "Ramya S","gajendrans@nmsworks.co.in","Dev");
+        Candidate c25 = new Candidate(25, "Neha S","gajendrans@nmsworks.co.in","QA");
+        Candidate c26 = new Candidate(26, "Sabarishwaran S","gajendrans@nmsworks.co.in","Dev");
+        Candidate c27 = new Candidate(27, "Elakkiya S","gajendrans@nmsworks.co.in","QA");
+        Candidate c28 = new Candidate(28, "Ellammal","gajendrans@nmsworks.co.in","Dev");
+        Candidate c29 = new Candidate(29, "Kavibharathi S","gajendrans@nmsworks.co.in","QA");
+        Candidate c30 = new Candidate(30, "Kabilan R","gajendrans@nmsworks.co.in","Dev");
+        Candidate c31 = new Candidate(31, "Velmurugan S","gajendrans@nmsworks.co.in","QA");
+        Candidate c32 = new Candidate(32, "Amarnath M","gajendrans@nmsworks.co.in","Dev");
+        Candidate c33 = new Candidate(33, "Vinothkumar E","gajendrans@nmsworks.co.in","QA");
+        Candidate c34 = new Candidate(34, "Kanagavel N","gajendrans@nmsworks.co.in","Dev");
+
+        Candidate[] candidates = new Candidate[14];
+        candidates[0] = c21;
+        candidates[1] = c22;
+        candidates[2] = c23;
+        candidates[3] = c24;
+        candidates[4] = c25;
+        candidates[5] = c26;
+        candidates[6] = c27;
+        candidates[7] = c28;
+        candidates[8] = c29;
+        candidates[9] = c30;
+        candidates[10] = c31;
+        candidates[11] = c32;
+        candidates[12] = c33;
+        candidates[13] = c34;
+
+        System.out.println(candidates.length);
+    }
+}
+
