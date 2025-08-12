3bd62035a578bbdec7943e57254dcb545a789529
Tharunkumar <tharunkumar@nmsworks.co.in>
2025-08-12 14:29:58 +0530

    Enum Practice
    Enum Practice code committed

--- tharun/src/main/java/in/co/nmsworks/runner/Runner.java
+++ tharun/src/main/java/in/co/nmsworks/runner/Runner.java
@@ -1,6 +1,7 @@
 package in.co.nmsworks.runner;
 
-import in.co.nmsworks.week1.day3.Candidate;
+import in.co.nmsworks.week2.day1.Subject;
+import in.co.nmsworks.week2.day1.Teacher;
 
 public class Runner {
     private static int age;
@@ -10,14 +11,33 @@ public class Runner {
         Double d = Double.valueOf(1.23);
         double d1 = d;
     }
+
+    public static void printTeacher(Subject subject){
+        System.out.println(subject.getTeacher().getName());
+    }
+
     public static void main(String[] args) {
 
-        age++;
+        Teacher teacher1 = new Teacher(1,"Sanjeev");
+        Teacher teacher2 = new Teacher(2,"Rajesh");
+
+        System.out.println(teacher1);
+        System.out.println(teacher2);
+
+        Subject maths = new Subject();
+        maths.setCode("MA101");
+        maths.setName("Maths");
+        maths.setTeacher(teacher1);
+
+        System.out.println(maths);
 
 
-        Sample s1=new Sample(1,"something");
-        System.out.println(s1);
 
-
+        Subject science = new Subject();
+        science.setCode("SE101");
+        science.setName("Science");
+        science.setTeacher(teacher2);
+        System.out.println(science);
+        printTeacher(science);
     }
 }
---
+++ tharun/src/main/java/in/co/nmsworks/week2/day2/EnumPractice.java
@@ -0,0 +1,49 @@
+package in.co.nmsworks.week2.day2;
+
+public class EnumPractice {
+    public enum Days{
+        SUNDAY("1st Day"),
+        MONDAY("2nd Day"),
+        TUESDAY("3rd Day"),
+        WEDNESDAY("4th Day"),
+        THURSDAY("5th Day"),
+        FRIDAY("6th Day"),
+        SATURDAY("7th Day");
+        final String description;
+
+        Days(String description) {
+            this.description = description;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        @Override
+        public String toString() {
+            return this.description;
+        }
+    }
+
+    static void printDayTypeAndDescription(Days day){
+
+        System.out.println(day+" "+day.name());
+
+        switch (day){
+            case SUNDAY :
+            case SATURDAY:
+                System.out.println(" Weekend " + day.description);
+                break;
+            default:
+                System.out.println("Week Day "+day.description);
+                break;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Days day=Days.SATURDAY;
+        printDayTypeAndDescription(day);
+    }
+
+}
