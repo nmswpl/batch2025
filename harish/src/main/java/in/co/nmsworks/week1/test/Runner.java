28570e87f1a7005deee792c43ea7438028b3610f
Harish <harishg@nmsworks.co.in>
2025-08-12 14:24:45 +0530

    Test Questions
    I this previuosly i used the different method to handle the checkout function now i changed it.

--- harish/src/main/java/in/co/nmsworks/week1/test/Runner.java
+++ harish/src/main/java/in/co/nmsworks/week1/test/Runner.java
@@ -8,30 +8,15 @@ public class Runner {
         tenthBook.setTitle("Physice");
         tenthBook.setPublicationYear(2019);
         tenthBook.setNoOfCopies(100);
-        checkOut(tenthBook);
+        tenthBook.checkOut();
 
         Book eleventhBook = new Book();
         eleventhBook.setAuthor("Tngoverment");
         eleventhBook.setTitle("Tamil");
         eleventhBook.setPublicationYear(2020);
         eleventhBook.setNoOfCopies(100);
-        checkOut(eleventhBook);
+        eleventhBook.checkOut();
 
 
-    }
-
-    public static void checkOut(Book bookname){
-        int numberOfCopies = bookname.getNoOfCopies() - 1;
-        if (numberOfCopies == 0){
-            System.out.println("There is no available copies of that book");
-        }
-        else{
-            System.out.println("The Title is "+bookname.getTitle());
-            System.out.println("The Author name is "+bookname.getAuthor());
-            System.out.println("The Publication year is "+bookname.getPublicationYear());
-            System.out.println("The Number of copies are availabel "+numberOfCopies);
-            System.out.println("The Author name is "+bookname.getAuthor());
-            System.out.println("\n");
-        }
     }
 }
