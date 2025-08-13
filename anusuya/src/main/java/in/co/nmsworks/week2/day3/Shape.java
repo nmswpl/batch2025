0dec3f2104810107b571efcde1d89579d10de115
AnusuyaDevi <anusuyadevir@nmsworks.co.in>
2025-08-13 14:32:19 +0530

    Practicing Interface and implements
    Practicing Shape interface and implements Rectangle, Triangle, Square classes

---
+++ anusuya/src/main/java/in/co/nmsworks/week2/day3/Shape.java
@@ -0,0 +1,12 @@
+package in.co.nmsworks.week2.day3;
+
+public interface Shape {
+
+    public int perimeter();
+    int area();
+    static final int A = 5;
+
+    default void printClassName(){
+        System.out.println(getClass().getSimpleName());
+    }
+}
