b39f971e10345e2f81bee6b815095abe7918fa37
Mohamed Asik <mohamedasikm@nmsworks.co.in>
2025-08-13 18:05:28 +0530

    Week 2 Day 3 Programs
    Implemented the given interface programs

---
+++ asik/src/main/java/in/co/nmsworks/week2/day3/CryptoPayment.java
@@ -0,0 +1,15 @@
+package in.co.nmsworks.week2.day3;
+
+public class CryptoPayment implements PaymentGateway
+{
+    @Override
+    public void processPayment(double amount)
+    {
+        System.out.println("Amount " + amount + " paid through " + toString());
+    }
+
+    @Override
+    public String toString() {
+        return "CryptoPayment";
+    }
+}
