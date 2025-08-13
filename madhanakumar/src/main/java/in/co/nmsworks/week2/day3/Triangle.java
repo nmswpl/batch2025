488daf74cd7a843d232163fc4194f2ab4ab5894b
Madhanakumar <madhanakumart@nmsworks.co.in>
2025-08-13 14:38:08 +0530

    Week 2 Day 3 programs
    Comitting the Interface example programs.

---
+++ madhanakumar/src/main/java/in/co/nmsworks/week2/day3/Triangle.java
@@ -0,0 +1,21 @@
+package in.co.nmsworks.week2.day3;
+
+public class Triangle implements Shape{
+    private int base;
+    private int height;
+
+    public Triangle(int base, int height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public int perimeter() {
+        return base + height;
+    }
+
+    @Override
+    public int area() {
+        return base * (height / 2);
+    }
+}
