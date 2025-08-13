3470d93b456ad44b5ca8cfdb44d67f988471a5d2
mahalakshmi <mahalakshmikarthiban@nmsworks.co.in>
2025-08-13 14:32:50 +0530

    day 3 of week 2
    committing day 3 of week 2 programs

---
+++ mahalakshmi/src/main/java/in/co/nmsworks/week2/day3/Rectangle.java
@@ -0,0 +1,26 @@
+package in.co.nmsworks.week2.day3;
+
+public class Rectangle implements Shape {
+    private int breadth;
+    private int length;
+
+
+    public Rectangle(int breadth, int length) {
+        this.breadth = breadth;
+        this.length = length;
+    }
+
+
+    @Override
+    public int perimeter() {
+        return (2 * (length+breadth));
+    }
+
+    @Override
+    public int area() {
+        return length * breadth;
+    }
+
+
+
+}
