6ae000f7a6efbc9d034af00b154ab71a478ece20
Mohamed Asik <mohamedasikm@nmsworks.co.in>
2025-08-13 14:31:20 +0530

    Week 2 Day 3 Programs
    Implemented the given interface programs

---
+++ asik/src/main/java/in/co/nmsworks/week2/day3/Square.java
@@ -0,0 +1,36 @@
+package in.co.nmsworks.week2.day3;
+
+public class Square implements Shape
+{
+    private int side;
+
+    public Square(int side)
+    {
+        this.side = side;
+    }
+
+    @Override
+    public int perimeter()
+    {
+        return 4 * side;
+    }
+
+    @Override
+    public int area()
+    {
+        return side * side;
+    }
+
+    @Override
+    public void printClassName() {
+        System.out.println(getClass().getName());;
+    }
+
+
+    public String getDescription()
+    {
+        return "This is a square";
+    }
+
+
+}
