b327a6b653a34ce73b422759c47b4679dcef0af1
vijayalakshmi <vijayalakshmir@nmsworks.co.in>
2025-08-14 09:33:47 +0530

    week 1 Test Program

--- viji/src/main/java/in/co/nmsworks/week1/test/HundredPositionWords.java
+++ viji/src/main/java/in/co/nmsworks/week1/test/HundredPositionWords.java
@@ -1,4 +1,6 @@
+
 // given a three digit number 542 and 777
+
 package in.co.nmsworks.week1.test;
 
 public class HundredPositionWords {
@@ -7,10 +9,36 @@ public class HundredPositionWords {
     }
 
     public static void printHundredPositionWords(int number){
-               if(number>100 && num<1000  ){
-
-
-
-               }
+      int num = number /100 ;
+      switch (num){
+          case 1 :
+            System.out.println("One");
+            break;
+          case 2 :
+              System.out.println("Two");
+              break;
+          case 3 :
+              System.out.println("Three");
+              break;
+          case 4 :
+              System.out.println("Four");
+              break;
+          case 5 :
+              System.out.println("Five");
+              break;
+          case 6 :
+              System.out.println("Six");
+              break;
+          case 7 :
+              System.out.println("Seven");
+              break;
+          case 8 :
+              System.out.println("Eight");
+              break;
+          case 9 :
+              System.out.println("Nine");
+              break;
+      }
     }
 }
+
