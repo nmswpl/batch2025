a9b3cfd52f0b4af164f027e5a4727c3d13206be1
Fiyaz <fiyazmohamed@nmsworks.co.in>
2025-08-14 10:33:19 +0530

    week 2 day 3 pending submitted

--- fiyaz/src/main/java/in/co/nmsworks/week2/day3/Multiplex.java
+++ fiyaz/src/main/java/in/co/nmsworks/week2/day3/Multiplex.java
@@ -1,6 +1,7 @@
 package in.co.nmsworks.week2.day3;
 
 public interface Multiplex {
+
     int bookTicket(int num);
     int checkAvailability(int ava);
 
--- fiyaz/src/main/java/in/co/nmsworks/week2/day3/Screen.java
+++ fiyaz/src/main/java/in/co/nmsworks/week2/day3/Screen.java
@@ -3,6 +3,7 @@ package in.co.nmsworks.week2.day3;
 public class Screen implements Multiplex {
 
     private int seatCount ;
+    private int seatNum = 1; ;
 
     public Screen(int seatCount) {
         this.seatCount = seatCount;
@@ -16,12 +17,20 @@ public class Screen implements Multiplex {
     @Override
     public int checkAvailability(int ava) {
         if (ava < seatCount) {
+            System.out.println();
             System.out.println("Yes Available" + "\n" + "Available Ticket : " + seatCount);
+            for (int i = 1; i <= ava; i++) {
+                System.out.print(seatNum++ + " ");
+            }
+            seatCount = seatCount - ava;
+            System.out.println();
+            System.out.println("Available Seat : " + seatCount);
         }
         else{
+            System.out.println();
             System.out.println("Ticket not Available " + "\n" + "Available Ticket : " + seatCount);
         }
 
-        return ava;
+        return seatNum;
     }
 }
