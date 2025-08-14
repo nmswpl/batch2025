5fa659ccd812b68becba670d9715a2e58fbf1642
Arsath2309 <ahamedarsath457@gmail.com>
2025-08-14 06:44:16 +0530

    Week 2 Day 3 Programming HW

---
+++ arsath/src/main/java/in/co/nmsworks/week2/day3/B.java
@@ -0,0 +1,6 @@
+package in.co.nmsworks.week2.day3;
+
+public interface B {
+    public void printName(String Name);
+    public int getId(int id);
+}
---
+++ arsath/src/main/java/in/co/nmsworks/week2/day3/Test.java
@@ -0,0 +1,13 @@
+package in.co.nmsworks.week2.day3;
+
+public class Test implements A,B{
+    @Override
+    public void printName(String Name) {
+
+    }
+
+    @Override
+    public int getId(int id) {
+        return 0;
+    }
+}
