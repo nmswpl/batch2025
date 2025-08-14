750d9235ea684efc686fdbd5fed06f617608fd7d
Gajendran <gajendrans@nmsworks.co.in>
2025-08-14 15:06:00 +0530

    Week 2 Day 1 practice toString
    task name create Subject class And Teacher Class
    commiting

---
+++ gajendran/src/main/java/in/co/nmsworks/week2/day1/Subject.java
@@ -0,0 +1,44 @@
+package in.co.nmsworks.week2.day1;
+
+public class Subject {
+
+    private String code, name;
+    private Teacher teacher;
+
+
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String toString() {
+        return "Subject{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", teacher=" + teacher +
+                '}';
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+}
