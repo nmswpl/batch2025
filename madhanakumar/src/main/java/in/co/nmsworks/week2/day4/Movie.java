82ac36c59598b0e9b2a0419a11fd27020c80fb20
Madhanakumar <madhanakumart@nmsworks.co.in>
2025-08-14 17:52:25 +0530

    Week 2 Day 4 programs Afternoon Session After Tea break Problem
    Comitting the MOVIE array problems. Inside the class six questions answered.

--- madhanakumar/src/main/java/in/co/nmsworks/week2/day4/Movie.java
+++ madhanakumar/src/main/java/in/co/nmsworks/week2/day4/Movie.java
@@ -36,8 +36,6 @@ public class Movie implements Comparable<Movie>{
                 '}';
     }
 
-
-
     @Override
     public int compareTo(Movie o) {
         if (this.yearOfRelease < o.yearOfRelease){
