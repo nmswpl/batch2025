33859022b01bf498fe58a6c7704eb56edfd9c4cb
Fiyaz <fiyazmohamed@nmsworks.co.in>
2025-08-14 15:20:34 +0530

    Sales and Inventory Reports
    Question is to practice about abstract.

---
+++ fiyaz/src/main/java/in/co/nmsworks/week2/day4/InventoryReport.java
@@ -0,0 +1,20 @@
+package in.co.nmsworks.week2.day4;
+
+public class InventoryReport extends Report {
+
+    @Override
+    public String header() {
+        return "InventoryReport";
+    }
+
+    @Override
+    public String body() {
+        return "";
+    }
+
+    @Override
+    public String footer() {
+        return "";
+    }
+
+}
