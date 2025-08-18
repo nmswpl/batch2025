0b83ceb1d76aa1dfb7a6950b19ae1c1c39726c11
vijayalakshmi <vijayalakshmir@nmsworks.co.in>
2025-08-18 10:55:01 +0530

    week 2  exam MyQueries

---
+++ viji/src/main/java/in/co/nmsworks/week2/exam/set2/MyQueries.sql
@@ -0,0 +1,27 @@
+--1. Change the location column’s data type to VARCHAR(100) to allow longer city names.
+alter table SOCIAL_MEDIA
+modify location varchar(100);
+
+--Add a column is_verified of type boolean to the SOCIAL_MEDIA table. The value
+  --should be false for all existing and new users unless changed explicitly.
+  alter table SOCIAL_MEDIA
+  add is_verified boolean false;
+  --Retrieve all users where the gender is 'Female' and the followers_count is either
+    --more than 10000 or the post_count is more than 1000.
+
+     select * from SOCIAL_MEDIA
+     where gender = "Female" and (followers_count >1000 or post_count>1000)
+
+     --Retrieve the users where the location is 'Hyderabad' or 'Kolkata', and the
+       --following_count is less than 1000.
+        select * from SOCIAL_MEDIA
+        where location = "Hyderabad" or location="Kolkata" and following_count < 1000 ;
+
+        --Find the location and gender wise number of like_count for each location in the
+         -- SOCIAL_MEDIA table. sort by location.
+
+
+
+
+
+
