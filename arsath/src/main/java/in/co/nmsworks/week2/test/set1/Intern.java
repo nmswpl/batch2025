27d4cebc96a1a29cfc421947c7b295a8369544e5
Arsath457 <arsathahameda@nmsworks.co.in>
2025-08-18 11:01:26 +0530

    Week 2 Test
    Question 2

---
+++ arsath/src/main/java/in/co/nmsworks/week2/test/set1/Intern.java
@@ -0,0 +1,32 @@
+package in.co.nmsworks.week2.test.set1;
+
+public class Intern {
+    private String name;
+    private String dept;
+
+    public Intern() {
+
+    }
+
+    public Intern(String name, String dept) {
+        this.name = name;
+        this.dept = dept;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+}
