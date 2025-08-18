2d72c9161f8e1c5368e87f841a509ba80ee71246
mathinath <mathinaths@nmsworks.co.in>
2025-08-18 11:09:51 +0530

    Exam Set-1
    successfully executed the updated Question 1 problem

--- mathinath/src/main/java/in/co/nmsworks/week2/exam/set1/DeviceUsage.java
+++ mathinath/src/main/java/in/co/nmsworks/week2/exam/set1/DeviceUsage.java
@@ -2,7 +2,8 @@ package in.co.nmsworks.week2.exam.set1;
 
 public class DeviceUsage {
     public static void main(String[] args) {
-        new DeviceUsage().printBatteryInfoAfterXHrsForDevice();
+        Phone p1 = new Phone();
+        new DeviceUsage().printBatteryInfoAfterXHrsForDevice(p1,5);
     }
     void printBatteryInfoAfterXHrsForDevice(BatteryPowered device, int
             noOfHrs) {
