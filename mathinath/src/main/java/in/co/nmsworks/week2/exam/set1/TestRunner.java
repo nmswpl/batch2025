83b090c6ae971b806d0a5677dd258bc9ee0a6802
mathinath <mathinaths@nmsworks.co.in>
2025-08-18 11:01:37 +0530

    Exam Set-1
    successfully executed the updated Question 3 problem

--- mathinath/src/main/java/in/co/nmsworks/week2/exam/set1/TestRunner.java
+++ mathinath/src/main/java/in/co/nmsworks/week2/exam/set1/TestRunner.java
@@ -1,14 +1,17 @@
 package in.co.nmsworks.week2.exam.set1;
 
 public class TestRunner {
+    public TestRunner() {
+
+    }
+
     public static void main(String[] args) {
         new TestRunner().convertTitleCase("java programming hello world");
         new TestRunner().printIntern();
     }
 
-    public  int  printIntern() {
 
-    }
+
 
     public void convertTitleCase(String str) {
         String result = "";
@@ -22,6 +25,38 @@ public class TestRunner {
         }
         System.out.println(result.trim());
 
+    }
+
+    public void printIntern() {
+        Intern i1 = new Intern("mathi","dev");
+        Intern i2 = new Intern("madan","dev");
+        Intern i3 = new Intern("subash","dev");
+        Intern[] intern = new Intern[] {i1,i2,i3};
+        for(int i=0;i<intern.length;i++)
+        {
+            System.out.println(i);
+        }
+
+
+    }
+
+}
+class Intern
+{
+    String name;
+    String dept;
+
+    public Intern(String name, String dept) {
+        this.name = name;
+        this.dept = dept;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDept() {
+        return dept;
     }
 
 }
