80785283eb6e2a0ae72768fb9bd61288e1aa14c9
Yamini <yamini@nmsworks.co.in>
2025-08-18 11:14:35 +0530

    Test set1
    Program for title case and return indices

---
+++ yamini/src/main/java/in/co/nmsworks/week2/test/set1/Queries.java
@@ -0,0 +1,12 @@
+package in.co.nmsworks.week2.test.set1;
+
+public class Queries {
+    //select*from SOCIAL_MEDIA where followers_count>50000 and location IN("Mumbai","Delhi");
+//alter table SOCIAL_MEDIA add column email varchar(100);
+//Query OK, 0 rows affected (0.05 sec)
+//Records: 0  Duplicates: 0  Warnings: 0
+   // alter table SOCIAL_MEDIA add column email varchar(100);
+   // Query OK, 0 rows affected (0.05 sec)
+   // Records: 0  Duplicates: 0  Warnings: 0
+
+}
