052b8b89da31873da70ce5c03113ebf059b555a3
Sheikfayas <sheikfayasg@nmsworks.co.in>
2025-08-18 11:03:40 +0530

    average salary and return int array
    commiting average salary and return int array

---
+++ sheik/src/main/java/in/co/nmsworks/week2/test/set2/TestRunner.java
@@ -0,0 +1,73 @@
+package in.co.nmsworks.week2.test.set2;
+
+public class TestRunner
+{
+
+    private String name;
+    private double salary;
+    public TestRunner() {
+    }
+
+    public TestRunner(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public static double averageSalary(TestRunner[] intern){
+        int sum =0 ;
+        for(int i=0;i<intern.length;i++){
+            sum = sum + intern.getSalary();
+        }
+        return sum/intern.length;
+    }
+
+    public static void main(String[] args) {
+        TestRunner interOne = new TestRunner("Fayas" , 2000.0d);
+        TestRunner interTwo = new TestRunner("Arun" , 3000.0d);
+        TestRunner interThree = new TestRunner("Ram" , 4000.0d);
+        TestRunner interFour = new TestRunner("Som" , 3000.0d);
+        TestRunner internFive = new TestRunner("Gokul" , 3500.0d);
+
+        TestRunner intern[] = {interOne,interTwo,interThree,interFour,internFive};
+
+        System.out.println(averageSalary(intern));
+
+
+        String para = "Java is programming language.C language was developed first.But java is popular language.";
+        System.out.println(paraInfo(para));
+        int [] info = paraInfo(para);
+        System.out.println(info);
+
+
+    }
+
+
+
+    public static int[] paraInfo(String para){
+        int[] arr = new int[3];
+        String[] statement = para.split(".");
+        arr[0] = statement.length;
+        String[] words =para.split(" ");
+        arr[1]= words.length;
+        int count =0 ;
+        for(int i=0;i<para.length();i++){
+            if(para.charAt(i)<='a' || para.charAt(i)>='z'){
+                count++;
+            }
+        }
+        arr[2] = count;;
+        return arr;
+
+    }
+
+
+
+}
