7c686aaeeec86f1b04b0c476365a4fcd0ef9c4b0
mahalakshmi <mahalakshmikarthiban@nmsworks.co.in>
2025-08-18 10:33:46 +0530

    Test question 2 (Index Printing)
    Committing print all indices of the interns that belong to a given department.

--- mahalakshmi/src/main/java/in/co/nmsworks/week2/test/set1/TestRunner.java
+++ mahalakshmi/src/main/java/in/co/nmsworks/week2/test/set1/TestRunner.java
@@ -10,8 +10,37 @@ public class TestRunner {
         }
         return res;
     }
-    public static void main(String[] args) {
-        String str1 = "This class should represent a drone device.";
-        System.out.println(convertTitleCase(str1));
+
+    public static int[] printIndices(Intern[] interns) {
+        int[] ans = new int[3];
+        int j = 0;
+        for(int i = 0 ; i < interns.length ; i++) {
+            if(interns[i].getDept().equals("Dev")) {
+                ans[j++] = i;
+            }
+        }
+        return ans;
     }
+
+    public static void main(String[] args) {
+//        String str1 = "This class should represent a drone device.";
+//        System.out.println(convertTitleCase(str1));
+
+        Intern intern1 = new Intern("Maha","Dev");
+        Intern intern2 = new Intern("Lokeshwari","Dev");
+        Intern intern3 = new Intern("Ramya","QA");
+        Intern intern4 = new Intern("Elakkiya","QA");
+        Intern intern5 = new Intern("Viji","Dev");
+
+        Intern[] interns = {intern1,intern2,intern3,intern4,intern5};
+        int[] res = printIndices(interns);
+        for (int i : res) {
+            System.out.print(i + " ");
+        }
+    }
+
+
+
+
+
 }
