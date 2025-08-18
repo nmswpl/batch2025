1cadbfe4cde6809eba9a54bdb1689fa6a9192140
Suddharshan <suddharshanv@nmsworks.co.in>
2025-08-18 11:10:49 +0530

    SQL done
    SQL programs done

---
+++ suddharshan/src/main/java/in/co/nmsworks/week2/test/set1/SQL.java
@@ -0,0 +1,12 @@
+package in.co.nmsworks.week2.test.set1;
+
+public class SQL {
+    /*
+    1. alter table SOCIAL_MEDIA add email varchar(100);
+    update SOCIAL_MEDIA set email = "username@example.com";
+
+    2. alter table SOCIAL_MEDIA modify location varchar(50);
+
+    3. select * from SOCIAL_MEDIA where followers_count > 5000 and (location = "Mumbai" or location = "Delhi");
+     */
+}
