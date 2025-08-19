a41da7f36691aeb4092348235b94d5f562ad44a3
Suddharshan <suddharshanv@nmsworks.co.in>
2025-08-19 13:54:05 +0530

    SQL Programs of Set 2 is completed
    SQL - Question 4 and 5 is done

--- suddharshan/src/main/java/in/co/nmsworks/week3/set2/answers.sql
+++ suddharshan/src/main/java/in/co/nmsworks/week3/set2/answers.sql
@@ -8,4 +8,8 @@ update SOCIAL_MEDIA set is_verified = false;
 -- 3. Retrieve all users where the gender is 'Female' and the followers_count is either more than 10000 or the post_count is more than 1000.
 select * from SOCIAL_MEDIA where gender = "Female" and (followers_count > 10000 or post_count > 1000);
 
--- Retrieve the users where the location is 'Hyderabad' or 'Kolkata', and the following_count is less than 1000.
+-- 4. Retrieve the users where the location is 'Hyderabad' or 'Kolkata', and the following_count is less than 1000.
+select * from SOCIAL_MEDIA where (location = "Hyderabad" or location = "Kolkata") and following_count < 1000;
+
+-- 5. Find the location and gender wise number of like_count for each location in the SOCIAL_MEDIA table sort by location
+select location, gender, count(like_count) from SOCIAL_MEDIA group by location, gender order by location;
