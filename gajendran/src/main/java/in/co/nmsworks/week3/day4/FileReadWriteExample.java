34982a34cb54fb01a4ff344e2ce6d1754fcbc77d
Gajendran <gajendrans@nmsworks.co.in>
2025-08-21 13:10:06 +0530

    week 3 day 4
    File Writing and Reading using Buffered Practice Task  FileReadWriteExample class commiting

--- gajendran/src/main/java/in/co/nmsworks/week3/day4/FileReadWriteExample.java
+++ gajendran/src/main/java/in/co/nmsworks/week3/day4/FileReadWriteExample.java
@@ -1,9 +1,7 @@
 package in.co.nmsworks.week3.day4;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import javax.sound.midi.Track;
+import java.io.*;
 
 public class FileReadWriteExample
 {
@@ -19,6 +17,39 @@ public class FileReadWriteExample
 
         fRWE.writeToFile("/tmp/myInfo.txt"," I studied at MIT Chennai");
         fRWE.readFromFile("/tmp/myInfo.txt");
+
+        System.out.println();
+        System.out.println("Buffer : \n");
+        fRWE.writeToFileUsingBuffer("/tmp/myInfo.txt","My Name Is Gsjendran I'm From Madurai");
+        fRWE.readFromFileUsingBuffer("/tmp/myInfo.txt");
+
+        fRWE.writeToFileUsingBuffer("/tmp/myInfo.txt"," I studied at MIT Chennai");
+        fRWE.readFromFileUsingBuffer("/tmp/myInfo.txt");
+    }
+
+    private void readFromFileUsingBuffer(String fileName)
+    {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName)))
+        {
+            String s;
+            System.out.println();
+            while( (s = bufferedReader.readLine()) != null){
+                System.out.print(s);
+            }
+        }catch (Exception e){
+            System.out.println("Error : "+e.toString());;
+        }
+    }
+
+    private void writeToFileUsingBuffer(String fileName, String text)
+    {
+        try (BufferedWriter bufferWriter = new BufferedWriter(new FileWriter(fileName, true))) {
+            bufferWriter.write(text);
+            bufferWriter.flush();
+        }catch (Exception e)
+        {
+            System.out.println("Error : "+e.toString());
+        }
     }
 
     private void readFromFile(String fileName)
