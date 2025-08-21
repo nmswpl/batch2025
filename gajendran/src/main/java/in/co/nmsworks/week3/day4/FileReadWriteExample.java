882f0c440b89426fb5f273b205e23a014c87938d
Gajendran <gajendrans@nmsworks.co.in>
2025-08-21 12:27:04 +0530

    week 3 day 4
    File Writing and Reading Practice Task  FileReadWriteExample class commiting

---
+++ gajendran/src/main/java/in/co/nmsworks/week3/day4/FileReadWriteExample.java
@@ -0,0 +1,52 @@
+package in.co.nmsworks.week3.day4;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileReadWriteExample
+{
+
+
+    public static void main(String[] args)
+    {
+        FileReadWriteExample fRWE = new FileReadWriteExample();
+
+        fRWE.writeToFile("/tmp/myInfo.txt","My Name Is Gsjendran I'm From Madurai");
+        fRWE.readFromFile("/tmp/myInfo.txt");
+
+
+        fRWE.writeToFile("/tmp/myInfo.txt"," I studied at MIT Chennai");
+        fRWE.readFromFile("/tmp/myInfo.txt");
+    }
+
+    private void readFromFile(String fileName)
+    {
+        try (FileReader reader = new FileReader(fileName))
+        {
+            int ch;
+            System.out.println("Content in File"+fileName);
+            while ((ch = reader.read()) != -1)
+            {
+                System.out.print((char) ch);
+            }
+            System.out.println("\n");
+        } catch (FileNotFoundException e)
+        {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void writeToFile(String fileName, String text)
+    {
+        try (FileWriter writer = new FileWriter(fileName, true))
+        {
+            writer.write(text);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
