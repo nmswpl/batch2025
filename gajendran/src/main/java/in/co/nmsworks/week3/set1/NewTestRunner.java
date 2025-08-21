a1892ace73548b72d275918a2b26edb057ca5e73
Gajendran <gajendrans@nmsworks.co.in>
2025-08-21 16:14:48 +0530

    week 3 day 1
    set 1 -  2 qn

---
+++ gajendran/src/main/java/in/co/nmsworks/week3/set1/NewTestRunner.java
@@ -0,0 +1,41 @@
+package in.co.nmsworks.week3.set1;
+
+import in.co.nmsworks.week2.day2.exam.set2.Intern;
+import in.co.nmsworks.week2.day4.Movie;
+
+import java.util.List;
+
+public class NewTestRunner
+{
+
+    public int[] fitchInternsInDept(InternNew[] interns, String dept)
+    {
+        int[] result = new int[5];
+        int indexOfArray = 0;
+        for (int i = 0; i < interns.length; i++) {
+            if (interns[i].getDept().equals(dept)){
+                result[indexOfArray] = i+1;
+                indexOfArray++;
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args)
+    {
+
+        NewTestRunner runner = new NewTestRunner();
+
+        InternNew[] interns = new InternNew[] {
+                new InternNew("Dev", "gajendran"),
+                new InternNew("Dev", "Alwin"),
+                new InternNew("Test", "hari"),
+                new InternNew("Test", "Ayyappan"),
+                new InternNew("Dev", "Santhosh"),
+                new InternNew("Test", "Deva")};
+        int[] indexOfDevIntern = runner.fitchInternsInDept(interns,"Dev");
+
+        for (int i : indexOfDevIntern) {
+            System.out.println(i);
+        }
+    }
+}
