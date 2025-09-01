1363db1a45d233e0ddded402fd94b6b51333bc99
Arsath457 <arsathahameda@nmsworks.co.in>
2025-09-01 14:19:50 +0530

    Week 5 Day 1
    Practice Problems

--- arsath/src/main/java/in/co/nmsworks/week5/day1/CardSimulator.java
+++ arsath/src/main/java/in/co/nmsworks/week5/day1/CardSimulator.java
@@ -4,71 +4,38 @@ import java.util.*;
 
 public class CardSimulator {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
         CardSimulator cs = new CardSimulator();
 
         List<String> players = new ArrayList<>();
-        players.add("Player A");
-        players.add("Player B");
-        players.add("Player C");
+
+        System.out.println("Enter the number of players: ");
+        int noOfPlayers = sc.nextInt();
+
+        for (int i = 1; i <= noOfPlayers ; i++) {
+            System.out.println("Enter the player " + i + " name : ");
+            String player = sc.next();
+            players.add(player);
+        }
+        System.out.println();
 
         List<Card> cards = new ArrayList<>();
 
-        cards.add(new Card("Spade",1));
-        cards.add(new Card("Spade",2));
-        cards.add(new Card("Spade",3));
-        cards.add(new Card("Spade",4));
-        cards.add(new Card("Spade",5));
-        cards.add(new Card("Spade",6));
-        cards.add(new Card("Spade",7));
-        cards.add(new Card("Spade",8));
-        cards.add(new Card("Spade",9));
-        cards.add(new Card("Spade",10));
-        cards.add(new Card("Spade",11));
-        cards.add(new Card("Spade",12));
-        cards.add(new Card("Spade",13));
+        for (int i = 1; i <=13 ; i++) {
+            cards.add(new Card("Spade",i));
+        }
 
-        cards.add(new Card("Clover",1));
-        cards.add(new Card("Clover",2));
-        cards.add(new Card("Clover",3));
-        cards.add(new Card("Clover",4));
-        cards.add(new Card("Clover",5));
-        cards.add(new Card("Clover",6));
-        cards.add(new Card("Clover",7));
-        cards.add(new Card("Clover",8));
-        cards.add(new Card("Clover",9));
-        cards.add(new Card("Clover",10));
-        cards.add(new Card("Clover",11));
-        cards.add(new Card("Clover",12));
-        cards.add(new Card("Clover",13));
+        for (int i = 1; i <=13 ; i++) {
+            cards.add(new Card("Clover",i));
+        }
 
-        cards.add(new Card("Heart",1));
-        cards.add(new Card("Heart",2));
-        cards.add(new Card("Heart",3));
-        cards.add(new Card("Heart",4));
-        cards.add(new Card("Heart",5));
-        cards.add(new Card("Heart",6));
-        cards.add(new Card("Heart",7));
-        cards.add(new Card("Heart",8));
-        cards.add(new Card("Heart",9));
-        cards.add(new Card("Heart",10));
-        cards.add(new Card("Heart",11));
-        cards.add(new Card("Heart",12));
-        cards.add(new Card("Heart",13));
+        for (int i = 1; i <=13 ; i++) {
+            cards.add(new Card("Heart",i));
+        }
 
-        cards.add(new Card("Diamond",1));
-        cards.add(new Card("Diamond",2));
-        cards.add(new Card("Diamond",3));
-        cards.add(new Card("Diamond",4));
-        cards.add(new Card("Diamond",5));
-        cards.add(new Card("Diamond",6));
-        cards.add(new Card("Diamond",7));
-        cards.add(new Card("Diamond",8));
-        cards.add(new Card("Diamond",9));
-        cards.add(new Card("Diamond",10));
-        cards.add(new Card("Diamond",11));
-        cards.add(new Card("Diamond",12));
-        cards.add(new Card("Diamond",13));
+        for (int i = 1; i <=13 ; i++) {
+            cards.add(new Card("Diamond",i));
+        }
 
         int cardsPerPlayer = cards.size()/ players.size();
 
@@ -86,7 +53,6 @@ public class CardSimulator {
                 cardsOfPlayer.add(cards.get(card++));
             }
             playerCards.put(player,cardsOfPlayer);
-
             System.out.println(player + " has " + playerCards.get(player));
             System.out.println();
         }
